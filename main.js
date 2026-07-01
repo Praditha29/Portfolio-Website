d04a1f7a2a4af99c29588c49bc4772aee721c435
Praditha29 <pradithareddy@gmail.com>
2026-07-01 12:22:36 +0530

    My Personal Website

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".text",{
+    strings:["Cybersecurity Student", "Game Developer", "Python Programmer"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+});
